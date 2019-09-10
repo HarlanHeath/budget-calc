6ffaef6f93b55700dd0a1dcc16c1acc4391a72b6
User <user@Users-MacBook-Pro.local>
2019-09-10 12:32:10 -0500

    setup form, item and list components, setup app to display information correctly before working on functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Alert from "./components/Alert";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseForm from "./components/ExpenseForm";
+import uuid from "uuid/v4"
+
+const initialExpenses = [
+  { id: uuid(), charge: "rent", amount: 1600 },
+  { id: uuid(), charge: "car payment", amount: 600 },
+  { id: uuid(), charge: "credit card bill", amount: 60 }
+]
 
 function App() {
+
+  const [expenses, setExpenses] = useState(initialExpenses);
+
   return (
     <React.Fragment>
       <Alert />
-      <ExpenseForm />
-      <ExpenseList />
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm />
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>total spending:
+        <span className="total">
+          ${expenses.reduce((acc, curr) => {
+            return acc += curr.amount
+          }, 0)}
+        </span>
+      </h1>
     </React.Fragment>
   );
 }
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import ExpenseItem from './ExpenseItem'
+import { MdDelete } from "react-icons/md"
 
-const Expenselist = () => {
+const Expenselist = ({ expenses }) => {
     return (
-        <div>
-            I'm an ExpenseList Component
-            <ExpenseItem />
-        </div>
+        <React.Fragment>
+            <ul className="list">
+                {expenses.map((expense) => {
+                    return <ExpenseItem key={expense.id} expense={expense} />
+                })}
+            </ul>
+            {expenses.length > 0 && <button className="btn">
+                clear expenses <MdDelete className="btn-icon" />
+            </button>}
+        </React.Fragment>
     )
 }
 
